fee78954ccf5eca5801ac7bda6531f9f7f4ae49a
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2025-05-24 16:39:12 +0600

    Updated Cache

--- backend/src/utils/cache.ts
+++ backend/src/utils/cache.ts
@@ -1,3 +1,7 @@
 import { Cache } from "@rediskit/cache";
 
-export const cache = new Cache();
+export const cache = new Cache({
+    host: process.env.REDIS_HOST || "localhost",
+    port: parseInt(process.env.REDIS_PORT || "6379", 10),
+    password: process.env.REDIS_PASSWORD || "awesomecoder",
+});
